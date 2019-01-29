08dfe3af7718277dcea980865e3a4fe6ea8a5940
siriphonnot <siriphonnot@gmail.com>
2019-01-30 00:41:20 +0700

    Add function encode

--- SimpleRest/SimpleRest.php
+++ SimpleRest/SimpleRest.php
@@ -57,4 +57,27 @@ class SimpleRest
       505 => 'HTTP Version Not Supported');
       return (isset($httpStatus[$statusCode])) ? $httpStatus[$statusCode] : 'Internal Server Error';
   }
+
+  public function encodeHtml($responseData) {
+		$htmlResponse = "<table border='1'>";
+		foreach($responseData as $key=>$value) {
+    			$htmlResponse .= "<tr><td>". $key. "</td><td>". $value. "</td></tr>";
+		}
+		$htmlResponse .= "</table>";
+		return $htmlResponse;		
+	}
+	
+	public function encodeJson($responseData) {
+		$jsonResponse = json_encode($responseData);
+		return $jsonResponse;		
+	}
+	
+	public function encodeXml($responseData) {
+		// creating object of SimpleXMLElement
+		$xml = new SimpleXMLElement('<?xml version="1.0"?><data></data>');
+		foreach($responseData as $key=>$value) {
+			$xml->addChild($key, $value);
+		}
+		return $xml->asXML();
+	}
 }
